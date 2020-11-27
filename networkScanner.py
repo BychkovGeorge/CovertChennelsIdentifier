082713b074975b70804f6b36f42c0b221bb29b20
g.bychkov <g.bychkov@grcc.tech>
2020-11-27 14:49:19 +0300

    WIP

--- networkScanner.py
+++ networkScanner.py
@@ -72,9 +72,10 @@ for j in range(len(list_of_network_addresses)):
             if len(nx.dijkstra_path(graph_tc, list_of_network_addresses[0], list_of_network_addresses[j])) == 2:
                 graph_tc.add_edge(str(list_of_network_addresses[0]), str(list_of_network_addresses[j]), color='r', weight=6)
             else:
-                for index in range(len(nx.dijkstra_path(graph_tc, list_of_network_addresses[0], list_of_network_addresses[j]))):
-                    if index != len(nx.dijkstra_path(graph_tc, list_of_network_addresses[0], list_of_network_addresses[j])) - 1:
-                        graph_tc.add_edge(str(nx.dijkstra_path(graph_tc, list_of_network_addresses[0], list_of_network_addresses[j])[index]), str(nx.dijkstra_path(graph_tc, list_of_network_addresses[0], list_of_network_addresses[j])[index + 1]), color='r', weight=6)
+                arr_of_tc_path = nx.dijkstra_path(graph_tc, list_of_network_addresses[0], list_of_network_addresses[j])
+                for index in range(len(arr_of_tc_path) - 1):
+                    if index != len(arr_of_tc_path) - 1:
+                        graph_tc.add_edge(str(arr_of_tc_path[index]), str(arr_of_tc_path[index + 1]), color='r', weight=6)
         if len(answer_fl[0]) != 0 and not hasattr(answer_fl[0][0][1], "type"):
             if len(nx.dijkstra_path(graph_fl, list_of_network_addresses[0], list_of_network_addresses[j])) == 2:
                 graph_fl.add_edge(str(list_of_network_addresses[0]), str(list_of_network_addresses[j]), color='r', weight=6)
